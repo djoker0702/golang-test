4ded040d397436be5b06179c06deab32c9fd99e1
jihed zouari <jihed.zouari@supcom.tn>
2018-01-25 16:57:49 +0100

    jihed

--- server/main.go
+++ server/main.go
@@ -29,20 +29,87 @@ func handleHttpRequest(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w,r,"../client/index.html")
 }
 
-type test_struct struct {
-	Test string
+
+type Data struct {
+	WebsiteUrl         string 
+	SessionId          string 
+	ResizeFrom         Dimension 
+	ResizeTo           Dimension
+	CopyAndPaste       map[string]bool// map[fieldId]true
+	FormCompletionTime int// Seconds
+}
+
+type Dimension struct {
+	Width  int 
+	Height int 
+}
+
+type event struct {
+	EventType		   string `json:"eventType"`
+	WebsiteUrl		   string `json:"websiteUrl"`
+	Session		   	   string `json:"sessionId"`
+	ODimension         Dimension `json:"ODimension"`
+	NDimension		   Dimension `json:"NDimension"`
+	Copie			   bool `json:"copied"`
+	Paste              bool `json:"pasted"`
+	FormId			   string `json:"formId"`
+	Time               int `json:"time"`
 }
 
 func eventHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("a post detected")
-	decoder := json.NewDecoder(r.Body)
-	var t test_struct
-	err := decoder.Decode(&t)
-	
-		if err != nil {
-			panic(err)
-		}
-		log.Println(t.Test)
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Unable to read body"))
+		return
+	}
+
+	req :=  &event{}
+	if err = json.Unmarshal(body, req); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Unable to unmarshal JSON request"))
+		return
+	}
+
+	mappedData := &Data{CopyAndPaste : map[string]bool {"inputEmail": false,"inputCardNumber": false,"inputCVV": false},
+	WebsiteUrl:  req.WebsiteUrl, SessionId:req.Session}
+
+	switch event := req.EventType; event {
+
+	case "screenResize" :
+		mappedData.ResizeFrom = req.ODimension	
+		mappedData.ResizeTo = req.NDimension
+		log.Printf("\nEvent : screenResize , Current state of the data :  %+v", 
+			"\nWebsiteUrl : ", mappedData.WebsiteUrl,
+			"\nSessionId : ", mappedData.SessionId,
+			"\nResizeFrom : ", mappedData.ResizeFrom,
+			"\nResizeTo : ", mappedData.ResizeTo)
+	case "timeTaken": 
+		mappedData.FormCompletionTime = req.Time
+		mappedData.ResizeFrom = req.ODimension	
+		mappedData.ResizeTo = req.NDimension
+		log.Printf("\nEvent : Form submitted , Final state of the data :  %+v", 
+			"\nWebsiteUrl : ", mappedData.WebsiteUrl,
+			"\nSessionId : ", mappedData.SessionId,
+			"\nResizeFrom : ", mappedData.ResizeFrom,
+			"\nResizeTo : ", mappedData.ResizeTo,
+			"\nFormCompletionTime : ", mappedData.FormCompletionTime)
+	case "copyAndPaste":
+		mappedData.CopyAndPaste[req.FormId] = req.Copie || req.Paste
+		log.Printf("\nEvent : Copy and Paste detected , Current state of the data :  %+v", 
+			"\nWebsiteUrl : ", mappedData.WebsiteUrl,
+			"\nSessionId : ", mappedData.SessionId,
+			"\nResizeFrom : ", mappedData.ResizeFrom,
+			"\nResizeTo : ", mappedData.ResizeTo,
+			"\nCopyAndPaste : ", mappedData.CopyAndPaste)
+
+	}
+
+	//log.Printf("Request received %+v", req)
+	//log.Printf("Mapped Data : %+v", mappedData)
+
+	w.WriteHeader(http.StatusOK)
     
 
 }
