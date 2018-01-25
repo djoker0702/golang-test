28b98b0a11acb89515b34bf8acab3819cff2016e
jihed zouari <jihed.zouari@supcom.tn>
2018-01-25 13:53:25 +0100

    jih

--- server/main.go
+++ server/main.go
@@ -11,11 +11,11 @@ import (
 func main() {
 
 	http.HandleFunc("/", handleHttpRequest)
-	//http.HandleFunc("/post", handlepost)
 	http.HandleFunc("/helloworld", helloWorldHandler)
-	fmt.Println("Server now running on localhost:8080")
-	fmt.Println(`Try running: curl -X POST -d '{"hello":"test123"}' http://localhost:8080/helloworld`)
-	log.Fatal(http.ListenAndServe(":8088", nil))
+	http.HandleFunc("/api/event", eventHandler)
+	fmt.Println("Server now running on localhost:3000")
+	fmt.Println(`Try running: curl -X POST -d '{"hello":"test123"}' http://localhost:3000/helloworld`)
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
 
 type helloWorldRequest struct {
@@ -28,15 +28,26 @@ func handleHttpRequest(w http.ResponseWriter, r *http.Request) {
 	
 		http.ServeFile(w,r,"../client/index.html")
 }
-/*func handlepost(w http.ResponseWriter, r *http.Request)  {
-			body, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				http.Error(w, "Error reading request body",
-					http.StatusInternalServerError)
-			}
-			log.Printf(string(body))
 
-}*/
+type test_struct struct {
+	Test string
+}
+
+func eventHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("a post detected")
+	decoder := json.NewDecoder(r.Body)
+	var t test_struct
+	err := decoder.Decode(&t)
+	
+		if err != nil {
+			panic(err)
+		}
+		log.Println(t.Test)
+    
+
+}
+
+
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
